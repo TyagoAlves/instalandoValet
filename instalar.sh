4a5d478d941ad30071e53b56c98b80f6bf1ddbce
Tyago_Alves <111445741+TyagoAlves@users.noreply.github.com>
2023-09-03 09:03:00 -0300

    Create instalar.sh
    É um shell script usando :
    
              cpriego/valet-linux
    
    Uma coisa necessario a fazer, é colocar o valet ao PATH,
    tem ser colocado manualmente acessando o  ".bashrc" e adicionando ao final o codigo:
    
              export PATH=$PATH:$HOME/gitpod/.config/composer/bin
    
    Em seguda é so colocar digitar o seguinte codigo no terminal:
    
              source ~/.bashrc

---
+++ instalar.sh
@@ -0,0 +1,48 @@
+
+#instalar php
+
+    sudo apt install php
+
+#intalar composer
+
+    php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+    php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+    php composer-setup.php
+    php -r "unlink('composer-setup.php');"
+    sudo mv composer.phar /usr/local/bin/composer
+
+#instalar nvm
+
+    sudo apt install curl
+    curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+    source ~/.bashrc
+    nvm install node
+    sudo apt install php-cli php-curl php-mbstring  php-xml php-zip
+    sudo apt-get install network-manager libnss3-tools jq xsel
+    sudo apt install php-sqlite3 php-mysql php-pgsql
+
+#intalar valet
+
+    composer global require cpriego/valet-linux
+    #adicionar valet ao PATH no .bashhrc:
+
+        cd ..
+        cd ..
+        #nano .bashrc
+            export PATH=$PATH:$HOME/gitpod/.config/composer/bin
+        source ~/.bashrc
+        sudo apt-get install nginx -y
+        /etc/init.d/apache2 stop
+        sudo apt install php8.1-fpm -y
+        sudo apt-get --purge remove apache2 -y
+        sudo systemctl disable apache2 
+        sudo apt austoremove -y
+        valet install 
+        valet restart
+        valet paths
+        sudo mkdir code
+        cd code
+        valet park
+        valet paths
+        sudo mkdir tyago
+        cd tyago
